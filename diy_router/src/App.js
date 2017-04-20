7ead18f091a1633cb850c8c3eaf254b063a3eee4
Charles Mead <meadch@gmail.com>
2017-04-20 10:56:59 -0700

    adds Link, wishes for Route

--- diy_router/src/App.js
+++ diy_router/src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component {
     render(){
         return (
             <div>
                 <Link to="/javascript">JavaScript</Link>
-                <a href="/haskell">Haskell</a><br />
-                <a href="/coffeescript">CoffeeScript</a><br />
-                <hr />
+                <Link to="/haskell">Haskell</Link>
+                <hr/>
+                <Route pattern='/javascript' component={JavaScript}/>
+                <Route pattern='/haskell' component={Haskell}/>
             </div>
         )
     }
 }
 
-const Link = (props) => {
-    return <h1>Link!</h1>
+const Link = ({to, children}) => {
+    const handleClick = (event) => {
+        event.preventDefault();
+        history.pushState({}, null, to);
+    }
+    return (
+        <a  
+            onClick={handleClick}
+            style={{display: 'block'}}
+            href={to}>
+            {children}
+        </a>
+    )
 }
 
+Link.propTypes = {
+    to: PropTypes.string.isRequired,
+    children: PropTypes.string.isRequired,
+}
+
+const JavaScript = () => <p>A high-level, dynamic, untyped, and interpreted programming language.</p>
+const Haskell = () => <p>A standardized, general-purpose purely functional programming language, with non-strict semantics and strong static typing.</p>
 
 export default App;
